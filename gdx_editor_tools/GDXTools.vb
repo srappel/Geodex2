19fb2e2eb566cd48afb8c7feab18996b6a69fa04
Stephen <srappel@uwm.edu>
2017-06-12 14:37:25 -0500

    Improves DMS readout, accounts for rounding errors

--- gdx_editor_tools/GDXTools.vb
+++ gdx_editor_tools/GDXTools.vb
@@ -59,21 +59,38 @@ Public Class GDXTools
     End Function
 
     Public Function FmtDegMinSec(ByVal DD As Single)
-        Dim deg As Integer
+        Dim deg As Double
         Dim minDouble As Double
-        Dim min As Integer
+        Dim min As Double
         Dim sec As Double
         Dim dms As String
+        Dim absDD As Double = Math.Abs(DD)
 
         'how to account for negative values indicating south and east?
         'Here, I will just use absolute values to determine, then we can handle the E and West in the fillform function
         'This is working well now.  Still need to account for E and W
 
-        deg = Math.Truncate(DD)
-        minDouble = (DD - deg) * 60
+        deg = Math.Truncate(absDD)
+        minDouble = (absDD - deg) * 60
         min = Math.Truncate(minDouble)
-        sec = Math.Round((minDouble - min) * 60, 2)
-        dms = Math.Abs(deg) & "° " & Math.Abs(min) & "' " & Math.Abs(sec) & "''"
+        sec = Math.Round((minDouble - min) * 60, 3)
+
+        'This If statement will evaluate numbers that should be clearly rounded to the nearest minute (seconds > 59.95 or < 0.05) where this error is clearly
+        'due to the repeating decimal.  The round error here will be less than the degrees of freedom.  0.05 seconds translates to less than 10 feet on most latitutes 
+        If Math.Abs(sec) < 0.05 And Math.Abs(sec) > 0 Then
+            'very close to the current minute
+            sec = 0
+        ElseIf Math.Abs(sec) > 59.95 Then
+            'Very close to the next higher minute
+            sec = 0
+            min += 1
+            'This will make sure that the minutes don't exceed 59
+            If min > 59 Then
+                deg += 1
+                min = 0
+            End If
+        End If
+        dms = Math.Abs(deg) & "° " & Math.Abs(min) & "' " & Math.Round(sec, 2) & "''"
         Return dms
 
     End Function
